80fd04b25e71d383a4c8c43481d77a28a7f918dc
Rogério Sobral Ribeiro <sobralcomix@gmail.com>
2025-07-08 09:07:51 -0300

    fetch(cores) implementando a classe que define as cores da aplicação

---
+++ utils/cores.py
@@ -0,0 +1,8 @@
+class Cores:
+    def __init__(self):
+        self.homefundo="#BED7FF"
+        self.corprimaria="#005394"
+        self.corsecundaria="#F68B18"
+        self.corbranca="#ffffff"
+        self.laranjaescuro="#FD6C21"
+        self.teste="#151342"
