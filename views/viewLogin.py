001cf18fbe39909b2ebe677d6bdb0fd643fc2d60
Erik de Sousa Gonçalves <erikdesousagoncalves1009@gmail.com>
2025-07-10 12:14:36 -0300

    fetch(viewLogin) Atualização da tela de login

--- views/viewLogin.py
+++ views/viewLogin.py
@@ -8,18 +8,32 @@ class ViewLogin(View):
         self.bgcolor=Cores().corbranca
         iconesenha=Icon(name=Icons.PASSWORD,color=Cores().corprimaria)
         iconelogin=Icon(name=Icons.LOGIN,color=Cores().corprimaria)
-        iconeimagem=Image(src="Senac_logo.svg.png",width=500,height=400)
+        iconeimagem=Image(src="Senac_logo.svg.png",width=500,height=400,col={"xs":9,"sm":6,"md":6,"lg":3.4,"xl":3.4})
         linhaimagem=ResponsiveRow(controls=[iconeimagem],alignment=MainAxisAlignment.CENTER)
-        self.login=TextField(prefix_icon=iconelogin,border_radius=10,label="Nome",color="black",col={"xs":2,"sm":2,"md":2,"lg":2,"xl":2},label_style=TextStyle(color="black"),width=1000)
-        self.password=TextField(prefix_icon=iconesenha,border_radius=10,label="Senha",color="black",password=True,can_reveal_password=True,col={"xs":2,"sm":2,"md":2,"lg":2,"xl":2},label_style=TextStyle(color="black"))
-        self.btnEnter=ElevatedButton(text="Entrar",width=150,height=50,col={"xs":12,"sm":8,"md":4,"lg":4,"xl":4},style=ButtonStyle(bgcolor={ControlState.HOVERED:Cores().laranjaescuro,
+        self.login=TextField(prefix_icon=iconelogin,border_radius=10,label="Registro",color="black",col={"xs":8,"sm":3,"md":4,"lg":3.4,"xl":3.4},label_style=TextStyle(color="black"),width=1000)
+        self.password=TextField(prefix_icon=iconesenha,border_radius=10,label="Senha",color="black",password=True,can_reveal_password=True,col={"xs":8,"sm":3,"md":4,"lg":3.4,"xl":3.4},label_style=TextStyle(color="black"))
+        self.btnEnter=ElevatedButton(text="Entrar",width=150,height=50,col={"xs":2.5,"sm":2.5,"md":3.2,"lg":1.5,"xl":1.5},style=ButtonStyle(bgcolor={ControlState.HOVERED:Cores().laranjaescuro,
         ControlState.FOCUSED:Colors.PINK,
         ControlState.DEFAULT:Cores().corsecundaria},color=Cores().corbranca,))
-        linha2=ResponsiveRow(controls=[self.login],alignment=MainAxisAlignment.CENTER)
-        linha4=ResponsiveRow(controls=[self.password],alignment=MainAxisAlignment.CENTER)
-        coluna=Column(controls=[linha2,linha4],spacing=30)
-        container=Container(content=coluna,margin=Margin(top=100,right=0,left=0,bottom=30))
-        linha5=Row(controls=[self.btnEnter],alignment=MainAxisAlignment.CENTER)
+        self.btnCadastro=TextButton(
+            text="Cadastrar",
+            style=ButtonStyle(color={ControlState.DEFAULT: Cores().corsecundaria,
+                                     ControlState.HOVERED: Cores().corprimaria,
+                                     ControlState.FOCUSED: Cores().corprimaria},
+                              bgcolor={ControlState.DEFAULT: Colors.TRANSPARENT,
+                                       ControlState.HOVERED: Colors.TRANSPARENT,
+                                       ControlState.FOCUSED: Colors.TRANSPARENT},
+                              overlay_color={ControlState.DEFAULT: Colors.TRANSPARENT,
+                                             ControlState.HOVERED: Colors.TRANSPARENT,
+                                             ControlState.FOCUSED: Colors.TRANSPARENT},text_style={
+            ControlState.DEFAULT: TextStyle(size=18)
+        }))
 
-        colum1=Column(controls=[linhaimagem,container,linha5],spacing=20)
+        linhalogin=ResponsiveRow(controls=[self.login],alignment=MainAxisAlignment.CENTER)
+        linhapassword=ResponsiveRow(controls=[self.password],alignment=MainAxisAlignment.CENTER)
+        coluna=Column(controls=[linhalogin,linhapassword],spacing=30)
+        container=Container(content=coluna,margin=Margin(top=100,right=0,left=0,bottom=30))
+        linhabotoes=ResponsiveRow(controls=[self.btnEnter,self.btnCadastro],alignment=MainAxisAlignment.CENTER)
+
+        colum1=Column(controls=[linhaimagem,container,linhabotoes],spacing=20)
         self.controls=[colum1]
