392f503eb9126a28056876d21e258f2a8fb487ff
Erik de Sousa Gonçalves <erikdesousagoncalves1009@gmail.com>
2025-07-10 12:16:47 -0300

    fetch(ControllerLogin) Implementando a tela de cadastro

--- controllers/controllerLogin.py
+++ controllers/controllerLogin.py
@@ -6,10 +6,15 @@ class ControllerLogin:
         self.login=telaLogin.login
         self.password=telaLogin.password
         self.btnEnter=telaLogin.btnEnter
+        self.btnCadastro=telaLogin.btnCadastro
         self.page=page
         self.btnEnter.on_click=self.handleEnter
+        self.btnCadastro.on_click=self.handleRegister
 
     def handleEnter(self,e):
-        if self.password.value=="1234" and self.login.value=="carlos":
-            print("estou dentro do evento")
+        # if self.password.value=="1234" and self.login.value=="carlos":
+        #     print("estou dentro do evento")
             self.page.go("/home")
+
+    def handleRegister(self,e):
+         self.page.go("/cadastro")
