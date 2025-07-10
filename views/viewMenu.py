d07a61c622963f132b80a4fdf6d18280ecbcbc1a
Erik de Sousa Gonçalves <erikdesousagoncalves1009@gmail.com>
2025-07-10 12:15:15 -0300

    fetch(viewMenu) Atualização da tela de menu

--- views/viewMenu.py
+++ views/viewMenu.py
@@ -6,145 +6,33 @@ class ViewMenu(View):
 
     def __init__(self):
         super().__init__()
-
         self.route=("/home")
+        self.img=Image(src="profile.png",width=200,height=200,col={"xs":3,"sm":3,"md":4,"lg":3.4,"xl":3.4})
+        self.imgS = Image(src="senac.png",width=200,height=200,col={"xs":3,"sm":3.5,"md":4,"lg":3.4,"xl":3.4})
+        iconepesquisa=Icon(name=Icons.SEARCH,color=Cores().corprimaria)
+        self.pesquisa=TextField(prefix_icon=iconepesquisa,border_radius=10,label="Pesquisar",color="black",col={"xs":8,"sm":4,"md":4,"lg":3.4,"xl":3.4},label_style=TextStyle(color="black"),width=1000,bgcolor=Cores().corbranca)
+        linhapesquisa=ResponsiveRow(controls=[self.img,self.imgS,self.pesquisa],alignment=MainAxisAlignment.CENTER,vertical_alignment=CrossAxisAlignment.CENTER)
+        self.btnVisualizar=ElevatedButton(text="Visualizar",width=150,height=50,col={"xs":4,"sm":4,"md":4,"lg":2.7,"xl":2.7},style=ButtonStyle(bgcolor={ControlState.HOVERED:Cores().laranjaescuro,
+        ControlState.FOCUSED:Colors.PINK,
+        ControlState.DEFAULT:Cores().corsecundaria},color=Cores().corbranca,))
+        self.btnGerenciar=ElevatedButton(text="Gerenciar",width=150,height=50,col={"xs":4,"sm":4,"md":4,"lg":2.7,"xl":2.7},style=ButtonStyle(bgcolor={ControlState.HOVERED:Cores().laranjaescuro,
+        ControlState.FOCUSED:Colors.PINK,
+        ControlState.DEFAULT:Cores().corsecundaria},color=Cores().corbranca,))
+        self.btnEntrada=ElevatedButton(text="Entradas",width=150,height=50,col={"xs":4,"sm":4,"md":4,"lg":2.7,"xl":2.7},style=ButtonStyle(bgcolor={ControlState.HOVERED:Cores().laranjaescuro,
+        ControlState.FOCUSED:Colors.PINK,
+        ControlState.DEFAULT:Cores().corsecundaria},color=Cores().corbranca,))
+        self.btnSaida=ElevatedButton(text="Saidas",width=150,height=50,col={"xs":4,"sm":4,"md":4,"lg":2.7,"xl":2.7},style=ButtonStyle(bgcolor={ControlState.HOVERED:Cores().laranjaescuro,
+        ControlState.FOCUSED:Colors.PINK,
+        ControlState.DEFAULT:Cores().corsecundaria},color=Cores().corbranca,))
+        linhabotoes=ResponsiveRow(controls=[self.btnVisualizar,self.btnGerenciar,self.btnEntrada,self.btnSaida],alignment=MainAxisAlignment.CENTER,vertical_alignment=CrossAxisAlignment.CENTER,spacing=20)
+        conteudo=Column(controls=[linhapesquisa,linhabotoes],spacing=100)
+        self.produtos=Column(controls=[Text("Vassoura",size=40),Text("Sabonete",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40),Text("Alvejante",size=40)],scroll=ScrollMode.AUTO)
+        conteinerprodutos=Container(content=self.produtos,alignment=alignment.center)
+        partedecima=Container(bgcolor=Cores().corbranca,width=3000,height=500,content=conteudo)
+        partedebaixo=Container(bgcolor=Cores().corprimaria,width=2000,height=1000,alignment=alignment.center,content=Container(bgcolor=Cores().corbranca,width=1200,height=900,border=border.all(2, Colors.BLACK),content=conteinerprodutos))
+        self.scroll=True
 
-        self.img=Image(src="profile.png")
-        self.imgS = Image(src="senac.png")
+   
+        
 
-        self.bloco = Container(
-            bgcolor=Cores().corprimaria,
-            width=1920,
-            height=300,
-            content=Column(
-                alignment=MainAxisAlignment.CENTER,
-                horizontal_alignment=CrossAxisAlignment.CENTER,
-                spacing=30,
-                controls=[
-                    Row(
-                        alignment=MainAxisAlignment.CENTER,
-                        spacing=100,
-                        controls=[
-                            Container(bgcolor="White", width=100, height=100, border_radius=100, content=self.img),
-                            Container(bgcolor=Cores().corprimaria, width=530, height=100, content=self.imgS),
-                            Container(bgcolor="White", width=450, height=40,border_radius=5,
-                                                        content=TextField(label="Procurar Produtos",
-                                                        bgcolor=Cores().homefundo,
-                                                        color="Black",
-                                                        hint_text="Buscando...",
-                                                        label_style= TextStyle(color="Black"))),
-                        ]
-                    ),
-                    Row(
-                        alignment=MainAxisAlignment.CENTER,
-                        spacing=10,
-                        controls=[
-                            ElevatedButton(
-                                bgcolor=Cores().corsecundaria,
-                                width=320,
-                                height=50,
-                                content=Text("Visualizar Estoque", text_align=center, color=Cores().corbranca)
-                            ),
-                            ElevatedButton(
-                                bgcolor=Cores().corsecundaria,
-                                width=320,
-                                height=50,
-                                content=Text("Gerenciar Produtos", text_align=center, color=Cores().corbranca)
-                            ),
-                            ElevatedButton(
-                                bgcolor=Cores().corsecundaria,
-                                width=320,
-                                height=50,
-                                content=Text("Entrada de Produto", text_align=center, color=Cores().corbranca)
-                            ),
-                            ElevatedButton(
-                                bgcolor=Cores().corsecundaria,
-                                width=320,
-                                height=50,
-                                content=Text("Saída de Produto", text_align=center, color=Cores().corbranca)
-                            ),
-                        ]
-                    )
-                ]
-            )
-        )
-
-        self.bloco2 = Container(
-            bgcolor=Cores().corbranca,
-            width=1920,
-            height=400,
-            alignment=alignment.center,
-            content=Container(
-                bgcolor=Cores().corprimaria,
-                width=1300,
-                height=350,
-                border=border.all(2, "Black"),
-                alignment=alignment.center,
-                    content=Column(
-                        spacing=5,
-                        scroll=ScrollMode.AUTO,
-                        controls=[
-
-                        Container(
-                        bgcolor=Cores().corbranca,
-                        width=1250,
-                        height=50,
-                        alignment=alignment.center,
-                            content=Container(
-                                bgcolor="Black",
-                                width=1230,
-                                height=40,
-                                alignment=alignment.center,
-                                    content=Text("Exemplo de Produto", color=Cores().corbranca)
-                            )
-                    ),
-                        Container(
-                        bgcolor=Cores().corbranca,
-                        width=1250,
-                        height=50,
-                        alignment=alignment.center
-                    ),
-                        Container(
-                        bgcolor=Cores().corbranca,
-                        width=1250,
-                        height=50,
-                        alignment=alignment.center
-                    ),
-                        Container(
-                        bgcolor=Cores().corbranca,
-                        width=1250,
-                        height=50,
-                        alignment=alignment.center
-                    ),
-                        Container(
-                        bgcolor=Cores().corbranca,
-                        width=1250,
-                        height=50,
-                        alignment=alignment.center
-
-                    ),  Container(
-                        bgcolor=Cores().corbranca,
-                        width=1250,
-                        height=50,
-                        alignment=alignment.center
-                    ),
-
-                        Container(
-                        bgcolor=Cores().corbranca,
-                        width=1250,
-                        height=50,
-                        alignment=alignment.center
-                    ),
-
-                        Container(
-                        bgcolor=Cores().corbranca,
-                        width=1250,
-                        height=50,
-                        alignment=alignment.center
-                    ),
-                    ]
-                )
-            )
-        )
-
-        self.controls=[self.bloco, self.bloco2]
+        self.controls=[partedecima,partedebaixo]
